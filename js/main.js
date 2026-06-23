f1593c3d5a3a22a0e0db24afec00a9746c5a7a23
Dev-Ethan1234 <ethannganga8@gmail.com>
2026-06-23 11:02:29 +0300

    Made changes to the navbar and footer

--- js/main.js
+++ js/main.js
@@ -18,4 +18,13 @@ function filterSchedule(material) {
             row.style.display = 'none'; 
         }
     });
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-shrink');
+    } else {
+        navbar.classList.remove('navbar-shrink');
+    }
+});
 }
